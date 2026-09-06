7b148e686e36e7195bfbee1779a542c63fa2ed7b
munch007 <broishan321@gmail.com>
2026-09-06 17:17:32 +0530

    Implement dictionary and tuple examples in Assigment1.py
    Added dictionary and tuple operations to demonstrate basic Python data structures.

--- Assigment1.py
+++ Assigment1.py
@@ -1,1 +1,33 @@
+dict = {'name': 'sanchit', 'prn':1272262076,'course':'core cse'}
+
+print(dict['name'])
+
+print(dict.keys())
+
+print(dict.values())
+
+print(dict.items())
+
+print('name' in dict)
+
+dict['year']=2026
+print(dict)
+
+dict['course']='btech cse core'
+print(dict)
+
+del dict['year']
+print(dict)
+
+tuple = ('sanchit',18,2007)
+
+print(tuple[0])
+
+print(tuple[:2])
+
+print(2007 in tuple)
+
+print(tuple.count(18))
+
+print(tuple.index(2007))
 
